46eaae133823e43c006ed954567ebe05bbbd2107
Felice <felicepng2010@gmail.com>
2022-10-03 21:43:58 +0800

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { MicroserviceOptions, Transport } from "@nestjs/microservices";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+
+    const user = process.env.RABBITMQ_USER;
+    const password = process.env.RABBITMQ_PASSWORD;
+    const host = process.env.RABBITMQ_HOST;
+    const queueName = process.env.RABBITMQ_QUEUE_NAME;
+
+    await app.connectMicroservice<MicroserviceOptions>({
+        transport: Transport.RMQ,
+        options: {
+            urls: [`amqp://${user}:${password}@${host}`],
+            queue: queueName,
+            queueOptions: {
+                durable: true,
+            },
+        },
+    });
+
+    app.startAllMicroservices();
 }
 bootstrap();
