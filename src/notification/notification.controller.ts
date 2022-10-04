48a603b750376d8d4362f415a3fca22181710e96
Ashley Lau <54572102+arshhlehhhh@users.noreply.github.com>
2022-10-04 17:14:33 +0800

    Add in event listener to subscribe to messages

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -1,9 +1,17 @@
 import { Controller, Get } from "@nestjs/common";
+import { EventPattern } from "@nestjs/microservices";
 import { NotificationService } from "./notification.service";
 
 @Controller("notification")
 export class NotificationController {
     constructor(private readonly notificationService: NotificationService) {}
+
+    // TODO: Add in corresponding event name - e.g. @EventPattern(send_email_notification)
+    @EventPattern()
+    async handleSendMessageEvent(data: Record<string, unknown>) {
+        // TODO: Implement handler function.
+        console.log(data);
+    }
 
     @Get()
     getHello(): string {
