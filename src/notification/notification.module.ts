7b038b25473a45494429ecc66a58faca4193e965
Felice <felicepng2010@gmail.com>
2022-10-09 00:01:03 +0800

    chore: remove repository file

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -1,11 +1,10 @@
 import { Module } from "@nestjs/common";
 import { NotificationController } from "./notification.controller";
-import { NotificationRepository } from "./notification.repository";
 import { NotificationService } from "./notification.service";
 
 @Module({
     imports: [],
     controllers: [NotificationController],
-    providers: [NotificationRepository, NotificationService],
+    providers: [NotificationService],
 })
 export class NotificationModule {}
