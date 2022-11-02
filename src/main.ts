c52d1fbff7a0d609ee50f0e8f760cf7f641c16a7
Felice <felicepng2010@gmail.com>
2022-11-02 13:15:43 +0800

    chore: update prod url

--- src/main.ts
+++ src/main.ts
@@ -7,16 +7,20 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const configService = app.get(ConfigService);
 
+    const method = configService.get("RABBITMQ_TRANSPORT_METHOD");
     const user = configService.get("RABBITMQ_USER");
     const password = configService.get("RABBITMQ_PASSWORD");
-    const host = configService.get("RABBITMQ_HOST");
+    const host =
+        process.env.NODE_ENV === "production"
+            ? configService.get("RABBITMQ_PROD_HOST")
+            : configService.get("RABBITMQ_HOST");
     const port = configService.get("RABBITMQ_PORT");
     const queueName = configService.get("RABBITMQ_QUEUE_NAME");
 
     const ms = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
         transport: Transport.RMQ,
         options: {
-            urls: [`amqps://${user}:${password}@${host}:${port}`],
+            urls: [`${method}://${user}:${password}@${host}:${port}`],
             queue: queueName,
             queueOptions: {
                 durable: true,
